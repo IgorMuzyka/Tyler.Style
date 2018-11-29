ec466208913f832b0e708fd9c6a8c26cde8ddd81
Igor Muzyka <imuz@ciklum.com>
2018-11-29 15:50:12 +0200

    Added init to Stylist Protocol definition

--- Sources/Style/Stylist.swift
+++ Sources/Style/Stylist.swift
@@ -9,6 +9,8 @@ public protocol Stylist {
 	func style(anyStylable: Any, style: Style, tags: [Tag], pair: VariableResolutionPair) throws
 
     static var keyPath: AnyKeyPath? { get }
+
+    init()
 }
 
 #endif
