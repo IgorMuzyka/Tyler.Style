87e1e163935220859de9f06843a10c6beeb18490
Igor Muzyka <imuz@ciklum.com>
2019-01-18 11:03:48 +0200

    Minor updates

--- Sources/Style/TypePreservingCodingAdapter+Convenience.swift
+++ Sources/Style/TypePreservingCodingAdapter+Convenience.swift
@@ -3,6 +3,7 @@ import TypePreservingCodingAdapter
 
 extension TypePreservingCodingAdapter {
 
+    @discardableResult
     public func register<GenericStyle: Style>(style: GenericStyle.Type) -> TypePreservingCodingAdapter {
         register(type: GenericStyle.self)
         register(alias: GenericStyle.alias, for: GenericStyle.self)
